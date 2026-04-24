6035232fba639ce40556ea492ff742982bf4b1a5
ariznurfaishal02-star <ariznurfaishal02@gmail.com>
2026-04-24 14:31:50 +0700

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import streamlit as st
+import os
 
 @st.cache_data
 def load_data():
     try:
-        df = pd.read_csv("Data_eBrix_Variatif_Kategori.csv", sep=",", decimal=".")
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(base_dir, "Data_eBrix_Variatif_Kategori.csv")
+        df = pd.read_csv(path, sep=",", decimal=".")
     except FileNotFoundError:
-        # Mengembalikan tabel kosong agar aplikasi tidak mati
         return pd.DataFrame(columns=['Tanggal', 'Kode_Blok', 'Latitude', 'Longitude', 'Nilai_Brix'])
-    
+
     df = df.dropna(subset=['Latitude', 'Longitude', 'Nilai_Brix'])
     if 'Tanggal' in df.columns:
         df['Tanggal'] = pd.to_datetime(df['Tanggal'])
